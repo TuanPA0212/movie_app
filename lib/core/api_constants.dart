7e5121b8e5b9f62cda7bf7d7fa159367eda2b863
Phan Tuan <phananhtuan439@gmail.com>
2025-03-13 15:34:34 +0700

    refactor environment setup

--- lib/core/api_constants.dart
+++ lib/core/api_constants.dart
@@ -1,8 +1,7 @@
-import 'package:movie_app/core/api_keys.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
-  static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String imageBaseUrl = "https://image.tmdb.org/t/p/original";
-  static String getApiKey() => APIKeys.apiKey;
-  static const String bigImageBaseUrl = "https://image.tmdb.org/t/p/w500";
+  static String? baseUrl = dotenv.env['BASE_URL'];
+  static String? imageBaseUrl = dotenv.env['IMAGE_URL'];
+  static String? apiKey = dotenv.env['API_KEY'];
 }
