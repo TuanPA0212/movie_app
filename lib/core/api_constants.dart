50b0ec3eda15dc86392b04d7db703fe50c027d0f
Phan Tuan <phananhtuan439@gmail.com>
2025-03-13 14:15:47 +0700

    Ignore API keys from Git tracking

--- lib/core/api_constants.dart
+++ lib/core/api_constants.dart
@@ -1,6 +1,8 @@
+import 'package:movie_app/core/api_keys.dart';
+
 class ApiConstants {
   static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String apiKey = "7c1efe05c5b96e5e96757cb942a84dbf";
   static const String imageBaseUrl = "https://image.tmdb.org/t/p/original";
+  static String getApiKey() => APIKeys.apiKey;
   static const String bigImageBaseUrl = "https://image.tmdb.org/t/p/w500";
 }
