93cf32eecaf0e98b9ded9c90a1fe60c142d0a908
Phan Tuan <phananhtuan439@gmail.com>
2025-03-14 17:45:08 +0700

    initial navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/viewmodels/movie_view_model.dart';
-import 'package:movie_app/views/home.dart';
+import 'package:movie_app/viewmodels/navigation_view_model.dart';
+import 'package:movie_app/views/home/home.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async{
+import 'core/theme/app_colors.dart';
+
+void main() async {
   await dotenv.load(fileName: ".env");
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => MovieViewModel()),
+        ChangeNotifierProvider(create: (_) => NavigationViewModel()),
       ],
       child: MyApp(),
     ),
@@ -23,6 +27,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue, // Màu chính
+        scaffoldBackgroundColor: Colors.black, // Màu nền
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.black,
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(fontSize: 18, color: Colors.white),
+          bodyMedium: TextStyle(fontSize: 16, color: Colors.white),
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: AppColors.secondary,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+        ),
+        useMaterial3: true
+      ),
       home: const Home(),
     );
   }
---
+++ lib/views/myNetflix/my_netflix.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyNetflix extends StatelessWidget {
+  const MyNetflix({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("My Netflix"),
+    );
+  }
+}
